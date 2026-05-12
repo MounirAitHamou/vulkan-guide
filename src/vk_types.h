c9e6b525911906536846dee8d5f26f9034643f19
Mounir Aït Hamou <maith079@uottawa.ca>
2026-05-12 14:00:56 -0400

    Add fmt

--- src/vk_types.h
+++ src/vk_types.h
@@ -1,8 +1,19 @@
-﻿// vulkan_guide.h : Include file for standard system include files,
-// or project specific include files.
+﻿#pragma once
 
-#pragma once
+#include <memory>
+#include <optional>
+#include <string>
+#include <vector>
+#include <span>
+#include <array>
+#include <functional>
+#include <deque>
 
 #include <vulkan/vulkan.h>
+#include <vulkan/vk_enum_string_helper.h>
+#include <vk_mem_alloc.h>
 
-//we will add our main reusable types here
+#include <fmt/core.h>
+
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
